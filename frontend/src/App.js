9a514d07c3000a737c95f6bf8e7327ad737bbfd7
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-12 21:46:23 +0600

    The index.js file and app component

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import App from "./App.css";
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>React Ecosystem</h1>
+        </div>
+    );
+};
+
+export default App;

0a1ed20c2088f89e31bbf957a570af71f236f541
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-13 22:50:27 +0600

    Connecting component to the store

--- frontend/src/todos/NewTodoForm.js
+++ frontend/src/todos/NewTodoForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import { createTodo } from "./actions";
 import "./NewTodoForm.css";
 
 const useInput = (initialValue) => {
@@ -12,15 +14,32 @@ const useInput = (initialValue) => {
     ];
 };
 
-const NewTodoForm = () => {
+const NewTodoForm = ({ todos, onCreatePresed }) => {
     const [inputProps, resetInput] = useInput("");
 
     return (
         <div className="new-todo-form ">
             <input className="new-todo-input" type="text" {...inputProps} placeholder="Type your new todo here"/>
-            <button className="new-todo-button">Create Todo</button>
+            <button 
+            onClick={() => {
+                const isDuplicated = todos.some(todo => todo.text === inputProps.value);
+                if (!isDuplicated) {
+                    onCreatePresed(inputProps.value);
+                    resetInput("");
+                }
+            }}
+            className="new-todo-button"
+            >Create Todo</button>
         </div>
     );
 };
 
-export default NewTodoForm;
+const mapStateToProps = (state) => ({
+    todos: state.todos
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    onCreatePresed: text => dispatch(createTodo(text))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);

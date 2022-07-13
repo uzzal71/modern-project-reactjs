e5aeb6b54e30f3f9175026521c5f0e78b6c2f9e6
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-13 23:27:07 +0600

    Redux DevTools

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -17,4 +17,8 @@ const persistConfig = {
 const rootReducer = combineReducers(reducers);
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const configureStore = () => createStore(persistedReducer);
+export const configureStore = () => createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS__EXTENSION__ &&
+    window.__REDUX_DEVTOOLS__EXTENSION__(),
+);

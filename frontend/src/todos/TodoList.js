85e8bac103cae8d96f166f9ed2a7d2e1463cf7a5
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-14 22:53:56 +0600

    Creating a thunk

--- frontend/src/todos/TodoList.js
+++ frontend/src/todos/TodoList.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
 import { removeTodo, markTodoAsCompleted } from './actions';
+import { displayAlert } from './thunks';
 import './TodoList.css';
 
 const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
@@ -24,6 +25,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
     onRemovePressed: text => dispatch(removeTodo(text)),
     onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
+    onDisplayAlertClick: text => dispatch(displayAlert(text))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);

3c06a953d4684205a994f57187535a4391a646e1
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-15 22:44:47 +0600

    Using thunks to delete server resources

--- frontend/src/todos/thunks.js
+++ frontend/src/todos/thunks.js
@@ -1,4 +1,4 @@
-import { loadTodosInProgress, loadTodosSuccess, loadTodosFailure, createTodo } from './actions';
+import { loadTodosInProgress, loadTodosSuccess, loadTodosFailure, createTodo, removeTodo } from './actions';
 
 export const loadTodos = () => async (dispatch, getState) => {
     try {
@@ -20,12 +20,26 @@ export const addTodoRequest = (text) => async (dispatch) => {
             headers: {
                 "Content-Type": "application/json"
             },
-            method: "POST",
+            method: "post",
             body
         });
 
         const todo = await response.json();
         dispatch(createTodo(todo));
+
+    } catch (e) {
+        dispatch(displayAlert(e));
+    }
+}
+
+export const removeTodoRequest = (id) => async (dispatch) => {
+    try {
+        const response = await fetch(`http://localhost:8080/todos/${id}`, {
+            method: "delete"
+        });
+
+        const removedTodo = await response.json();
+        dispatch(removeTodo(removedTodo));
 
     } catch (e) {
         dispatch(displayAlert(e));
